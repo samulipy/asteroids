db17dfcf0c1eb838c5b1d497d455f33b0014d612
samulipy <samulipy@gmail.com>
2025-01-25 10:17:15 +0200

    Resitricted to 60 FPS

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
-        pygame.display.flip() 
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
